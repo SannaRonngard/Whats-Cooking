f9fdb184c2566a32e61f88fd5bc703ff471342c1
Soffylicious <sofya.larsson@gmail.com>
2017-03-08 14:36:51 +0100

    Jag gjorde en klass :100:

---
+++ src/whatscooking/SofiaTest.java
@@ -0,0 +1,12 @@
+package whatscooking;
+
+import javax.swing.JOptionPane;
+
+public class SofiaTest {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Hello World" );
+	}
+
+}
