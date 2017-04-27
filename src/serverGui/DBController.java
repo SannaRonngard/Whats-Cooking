d8b51472005985f9239779a3acc2a2d066de69cd
Soffylicious <sofya.larsson@gmail.com>
2017-04-27 20:54:37 +0200

    Logga in med "user" och "password"

--- src/serverGui/DBController.java
+++ src/serverGui/DBController.java
@@ -1,8 +1,12 @@
 package serverGui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class DBController {
 	
@@ -13,10 +17,19 @@ public class DBController {
 		@FXML
 		private PasswordField txtPassword;
 		
-		public void Login(ActionEvent event){
+		public void Login(ActionEvent event) throws Exception {
 			if(txtUsername.getText().equals("user") && txtPassword.getText().equals("password")){
 				lblStatus.setTextFill(Color.web("#43af43"));
 				lblStatus.setText("Log in successful!");
+				/*
+				 * Om villkoret stämmer öppnas nästa scen. 
+				 */
+				Stage primaryStage = new Stage();
+				Parent root = FXMLLoader.load( getClass().getResource("/serverGui/Menu.fxml"));
+				Scene scene = new Scene(root,650,500);
+				scene.getStylesheets().add(getClass().getResource("serverGui.css").toExternalForm());
+				primaryStage.setScene(scene);
+				primaryStage.show();
 			} else {
 				lblStatus.setTextFill(Color.web("#b22222"));
 				lblStatus.setText("Username or password wrong. Try again!");
