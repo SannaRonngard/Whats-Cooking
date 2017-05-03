c3140fa3210f79ad64b840ad37baeb672b6ff886
Soffylicious <sofya.larsson@gmail.com>
2017-05-03 14:30:38 +0200

    DB

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,6 +1,7 @@
 package database;
 
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Connection;
 /**
  * 
@@ -36,16 +37,31 @@ public class DBConnection {
 	   public static void main(String args[]) {
 		   DBConnection db = new DBConnection();
 		   Connection connection = null;
+		   // Try for Driver
 		   try {
-			   Class.forName(db.driver);
-		       connection = DriverManager.getConnection(db.getUrl(),db.getUsername(), db.getPassword());
-		      } catch (Exception e) {
-		         e.printStackTrace();
-		         System.err.println(e.getClass().getName()+": "+e.getMessage());
-		         System.exit(0);
-		      }
-		      System.out.println("Opened database successfully");
-		   }
+				Class.forName(db.driver);
+			} catch (ClassNotFoundException e) {
+				System.out.println("Where is your PostgreSQL JDBC Driver? Include in your library path!");
+				e.printStackTrace();
+				return;
+			}
+		   //Try to connect to database with details
+		   try {
+				connection = DriverManager.getConnection(db.getUrl(), db.getUsername(),db.getPassword());
+
+			} catch (SQLException e) {
+				System.out.println("Connection Failed! Check output console");
+				e.printStackTrace();
+				return;
+			}
+		   //If user details match and connection was successful or not
+		   if (connection != null) {
+				System.out.println("You are connected!");
+			} else {
+				System.out.println("Failed to make connection!");
+			}
+		   
+	   }
 	   
 }
 
