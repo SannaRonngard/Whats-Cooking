84368dd612d7e266d839d1cd464d71f674559149
jullemeister <olsson_olle@telia.com>
2017-05-18 12:11:32 +0200

    test

--- src/control/ApplicationInformation.java
+++ src/control/ApplicationInformation.java
@@ -4,19 +4,13 @@ import javafx.scene.control.Label;
 
 public class ApplicationInformation {
 	
-	public static void addApplicationInformation(Label label) {
-		Label applicationInformation = new Label(Words); 
-		applicationInformation.setWrapText(true);
-		
-	}
-	
-	public static void words(String string) {
+	public String words() {
 		String Words = 
 				"Welome To Whats Cooking\n"
 				+ "\n"
 				+ "This application is supposed to help our users to be one step ahead\n"
 				+ "when it comes to our daily rutines and make life a little bit simpler";
-		
+		return Words;
 	}
 
 }
